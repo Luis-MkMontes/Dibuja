a7ec376e8e77774fc57700250985360e09d0c675
Luis-MkMontes <l20211814@tectijuana.edu.mx>
2021-05-14 13:11:36 -0700

    Dibuja Figuras
    Tarea de Unidad 3 y 4

---
+++ Draw/dibujo1/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿
+namespace dibujo1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.cambiarColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rojoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.azulToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.verdeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.naranjaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.blancoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.figuraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rectanguloToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.elipseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lineaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.herramientasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dibujarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.seleccionarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.borrarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.borrarTodoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.borrarUltimoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.borrarSeleccionadoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ordenarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.caminoCircularToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.herramientasToolStripMenuItem,
+            this.figuraToolStripMenuItem,
+            this.cambiarColorToolStripMenuItem,
+            this.borrarToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // cambiarColorToolStripMenuItem
+            // 
+            this.cambiarColorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rojoToolStripMenuItem,
+            this.azulToolStripMenuItem,
+            this.verdeToolStripMenuItem,
+            this.naranjaToolStripMenuItem,
+            this.blancoToolStripMenuItem});
+            this.cambiarColorToolStripMenuItem.Name = "cambiarColorToolStripMenuItem";
+            this.cambiarColorToolStripMenuItem.Size = new System.Drawing.Size(152, 20);
+            this.cambiarColorToolStripMenuItem.Text = "Cambiar color de relleno:";
+            // 
+            // rojoToolStripMenuItem
+            // 
+            this.rojoToolStripMenuItem.Name = "rojoToolStripMenuItem";
+            this.rojoToolStripMenuItem.Size = new System.Drawing.Size(115, 22);
+            this.rojoToolStripMenuItem.Text = "Rojo";
+            this.rojoToolStripMenuItem.Click += new System.EventHandler(this.rojoToolStripMenuItem_Click);
+            // 
+            // azulToolStripMenuItem
+            // 
+            this.azulToolStripMenuItem.Name = "azulToolStripMenuItem";
+            this.azulToolStripMenuItem.Size = new System.Drawing.Size(115, 22);
+            this.azulToolStripMenuItem.Text = "Azul";
+            this.azulToolStripMenuItem.Click += new System.EventHandler(this.azulToolStripMenuItem_Click);
+            // 
+            // verdeToolStripMenuItem
+            // 
+            this.verdeToolStripMenuItem.Name = "verdeToolStripMenuItem";
+            this.verdeToolStripMenuItem.Size = new System.Drawing.Size(115, 22);
+            this.verdeToolStripMenuItem.Text = "Verde";
+            this.verdeToolStripMenuItem.Click += new System.EventHandler(this.verdeToolStripMenuItem_Click);
+            // 
+            // naranjaToolStripMenuItem
+            // 
+            this.naranjaToolStripMenuItem.Name = "naranjaToolStripMenuItem";
+            this.naranjaToolStripMenuItem.Size = new System.Drawing.Size(115, 22);
+            this.naranjaToolStripMenuItem.Text = "Naranja";
+            this.naranjaToolStripMenuItem.Click += new System.EventHandler(this.naranjaToolStripMenuItem_Click);
+            // 
+            // blancoToolStripMenuItem
+            // 
+            this.blancoToolStripMenuItem.Name = "blancoToolStripMenuItem";
+            this.blancoToolStripMenuItem.Size = new System.Drawing.Size(115, 22);
+            this.blancoToolStripMenuItem.Text = "Blanco";
+            this.blancoToolStripMenuItem.Click += new System.EventHandler(this.blancoToolStripMenuItem_Click);
+            // 
+            // figuraToolStripMenuItem
+            // 
+            this.figuraToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rectanguloToolStripMenuItem,
+            this.elipseToolStripMenuItem,
+            this.lineaToolStripMenuItem,
+            this.caminoCircularToolStripMenuItem});
+            this.figuraToolStripMenuItem.Name = "figuraToolStripMenuItem";
+            this.figuraToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.figuraToolStripMenuItem.Text = "Figura:";
+            // 
+            // rectanguloToolStripMenuItem
+            // 
+            this.rectanguloToolStripMenuItem.Name = "rectanguloToolStripMenuItem";
+            this.rectanguloToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
+            this.rectanguloToolStripMenuItem.Text = "Rectangulo";
+            this.rectanguloToolStripMenuItem.Click += new System.EventHandler(this.rectanguloToolStripMenuItem_Click);
+            // 
+            // elipseToolStripMenuItem
+            // 
+            this.elipseToolStripMenuItem.Name = "elipseToolStripMenuItem";
+            this.elipseToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
+            this.elipseToolStripMenuItem.Text = "Elipse";
+            this.elipseToolStripMenuItem.Click += new System.EventHandler(this.elipseToolStripMenuItem_Click);
+            // 
+            // lineaToolStripMenuItem
+            // 
+            this.lineaToolStripMenuItem.Name = "lineaToolStripMenuItem";
+            this.lineaToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
+            this.lineaToolStripMenuItem.Text = "Linea";
+            this.lineaToolStripMenuItem.Click += new System.EventHandler(this.lineaToolStripMenuItem_Click);
+            // 
+            // herramientasToolStripMenuItem
+            // 
+            this.herramientasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dibujarToolStripMenuItem,
+            this.seleccionarToolStripMenuItem,
+            this.ordenarToolStripMenuItem});
+            this.herramientasToolStripMenuItem.Name = "herramientasToolStripMenuItem";
+            this.herramientasToolStripMenuItem.Size = new System.Drawing.Size(93, 20);
+            this.herramientasToolStripMenuItem.Text = "Herramientas:";
+            // 
+            // dibujarToolStripMenuItem
+            // 
+            this.dibujarToolStripMenuItem.Name = "dibujarToolStripMenuItem";
+            this.dibujarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.dibujarToolStripMenuItem.Text = "Dibujar";
+            this.dibujarToolStripMenuItem.Click += new System.EventHandler(this.dibujarToolStripMenuItem_Click);
+            // 
+            // seleccionarToolStripMenuItem
+            // 
+            this.seleccionarToolStripMenuItem.Name = "seleccionarToolStripMenuItem";
+            this.seleccionarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.seleccionarToolStripMenuItem.Text = "Seleccionar";
+            this.seleccionarToolStripMenuItem.Click += new System.EventHandler(this.seleccionarToolStripMenuItem_Click);
+            // 
+            // borrarToolStripMenuItem
+            // 
+            this.borrarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.borrarTodoToolStripMenuItem,
+            this.borrarUltimoToolStripMenuItem,
+            this.borrarSeleccionadoToolStripMenuItem});
+            this.borrarToolStripMenuItem.Name = "borrarToolStripMenuItem";
+            this.borrarToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.borrarToolStripMenuItem.Text = "Borrar";
+            // 
+            // borrarTodoToolStripMenuItem
+            // 
+            this.borrarTodoToolStripMenuItem.Name = "borrarTodoToolStripMenuItem";
+            this.borrarTodoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.borrarTodoToolStripMenuItem.Text = "Borrar Todo";
+            this.borrarTodoToolStripMenuItem.Click += new System.EventHandler(this.borrarTodoToolStripMenuItem_Click);
+            // 
+            // borrarUltimoToolStripMenuItem
+            // 
+            this.borrarUltimoToolStripMenuItem.Name = "borrarUltimoToolStripMenuItem";
+            this.borrarUltimoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.borrarUltimoToolStripMenuItem.Text = "Borrar ultimo";
+            this.borrarUltimoToolStripMenuItem.Click += new System.EventHandler(this.borrarUltimoToolStripMenuItem_Click);
+            // 
+            // borrarSeleccionadoToolStripMenuItem
+            // 
+            this.borrarSeleccionadoToolStripMenuItem.Name = "borrarSeleccionadoToolStripMenuItem";
+            this.borrarSeleccionadoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.borrarSeleccionadoToolStripMenuItem.Text = "Borrar Seleccionado";
+            this.borrarSeleccionadoToolStripMenuItem.Click += new System.EventHandler(this.borrarSeleccionadoToolStripMenuItem_Click);
+            // 
+            // ordenarToolStripMenuItem
+            // 
+            this.ordenarToolStripMenuItem.Name = "ordenarToolStripMenuItem";
+            this.ordenarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ordenarToolStripMenuItem.Text = "Ordenar";
+            this.ordenarToolStripMenuItem.Click += new System.EventHandler(this.ordenarToolStripMenuItem_Click);
+            // 
+            // caminoCircularToolStripMenuItem
+            // 
+            this.caminoCircularToolStripMenuItem.Name = "caminoCircularToolStripMenuItem";
+            this.caminoCircularToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.caminoCircularToolStripMenuItem.Text = "Camino circular";
+            this.caminoCircularToolStripMenuItem.Click += new System.EventHandler(this.caminoCircularToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem cambiarColorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rojoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem azulToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem verdeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem naranjaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem blancoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem figuraToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rectanguloToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem elipseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lineaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem herramientasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dibujarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem seleccionarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem borrarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem borrarTodoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem borrarUltimoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem borrarSeleccionadoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ordenarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem caminoCircularToolStripMenuItem;
+    }
+}
+
---
+++ Draw/dibujo1/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dibujo1
+{
+    // Montes Ojeda Luis Antonio 20211814
+
+    struct Punto
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Punto(int X, int Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        List<Figura> figuras = new List<Figura>();
+        private string estado = "dibujando";
+        private string estadoFigura = "Rectangulo";
+
+        private Punto p1_actual;
+        private Figura figuraSeleccionada;
+
+        private Figura Selecciona(int x, int y)
+        {
+            for (int i = figuras.Count - 1; i >= 0; i--)
+            {
+                if (figuras[i].EstaDentro(x, y))
+                {
+                    return figuras[i];
+                }
+            }
+            return null;
+        }   
+
+        private void DibujaFiguras()
+        {
+            foreach (var figura in figuras)
+            {
+                figura.Dibuja(this);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (estado == "dibujando")
+            {
+                estado = "moviendo";
+                label1.Text = String.Format($"x:{e.X}   y:{e.Y}");
+                p1_actual = new Punto(e.X, e.Y);
+            }
+            else if (estado == "seleccionando")
+            {
+                figuraSeleccionada = Selecciona(e.X, e.Y);
+
+                if (figuraSeleccionada != null)
+                {
+                    label1.Text = String.Format($"x:{figuraSeleccionada.punto1.X}   y:{figuraSeleccionada.punto1.Y}");
+                }
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (estado == "moviendo")
+            {
+                estado = "dibujando";
+                label1.Text = String.Format($"x:{e.X}   y:{e.Y}");
+
+                if (estadoFigura == "Rectangulo")
+                {       
+                    if (e.Button == MouseButtons.Left)
+                    {
+                        Rectangulo r = new Rectangulo(p1_actual, new Punto(e.X, e.Y));
+                        r.Dibuja(this);
+                        figuras.Add(r);
+                    }
+                }
+                else if (estadoFigura == "Elipse")
+                {
+                    if (e.Button == MouseButtons.Left)
+                    {
+                        Elipse elipse = new Elipse(p1_actual, new Punto(e.X, e.Y));
+                        elipse.Dibuja(this);
+                        figuras.Add(elipse);
+                    }
+                }
+                else if (estadoFigura == "Linea")
+                {
+                    if (e.Button == MouseButtons.Left)
+                    {
+                        Linea linea = new Linea(p1_actual, new Punto(e.X, e.Y));
+                        linea.Dibuja(this);
+                        figuras.Add(linea);
+                    }
+                }
+                else if (estadoFigura == "Circulo")
+                {
+                    if (e.Button == MouseButtons.Left)
+                    {
+                        CaminoCircular circulo = new CaminoCircular(p1_actual, new Punto(e.X, e.Y));
+                        circulo.Dibuja(this);
+                        figuras.Add(circulo);
+                    }
+                }
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (estado == "dibujando")
+            {
+                label1.Text = String.Format($"x:{e.X}   y:{e.Y}");
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            this.DibujaFiguras();
+        }
+
+        private void dibujarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            estado = "dibujando";
+        }
+
+        private void seleccionarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            estado = "seleccionando";
+        }
+
+        private void ordenarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            figuras.Sort();
+            figuras.Reverse();
+            DibujaFiguras();
+        }
+
+        private void rojoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (figuraSeleccionada != null)
+            {
+                figuraSeleccionada.colorRelleno = Color.Red;
+                this.Refresh();
+            }
+        }
+
+        private void azulToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (figuraSeleccionada != null)
+            {
+                figuraSeleccionada.colorRelleno = Color.Blue;
+                this.Refresh();
+            }
+        }
+
+        private void verdeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (figuraSeleccionada != null)
+            {
+                figuraSeleccionada.colorRelleno = Color.Green;
+                this.Refresh();
+            }
+        }
+
+        private void naranjaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (figuraSeleccionada != null)
+            {
+                figuraSeleccionada.colorRelleno = Color.Orange;
+                this.Refresh();
+            }
+        }
+
+        private void blancoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (figuraSeleccionada != null)
+            {
+                figuraSeleccionada.colorRelleno = Color.White;
+                this.Refresh();
+            }
+        }
+
+        private void rectanguloToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            estadoFigura = "Rectangulo";
+        }
+
+        private void elipseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            estadoFigura = "Elipse";
+        }
+
+        private void lineaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            estadoFigura = "Linea";
+        }
+        private void caminoCircularToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            estadoFigura = "Circulo";
+        }
+
+        private void borrarTodoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            figuras.Clear();
+            Refresh();
+        }
+
+        private void borrarUltimoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int i = figuras.Count - 1;
+
+            if (i >= 0)
+            {
+                figuras.RemoveAt(i);
+                Refresh();
+            }
+        }
+
+        private void borrarSeleccionadoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (figuraSeleccionada != null)
+            {
+                figuras.Remove(figuraSeleccionada);
+                Refresh();
+            }
+        }
+    }
+}
